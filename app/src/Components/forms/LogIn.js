e80fb81b9b070d5f9ec3eddcf22313d48c31f814
Omar <mromarhesham1999@gmail.com>
2023-05-03 17:44:30 +0300

    Form submission and getting user inputs in login form

--- app/src/Components/forms/LogIn.js
+++ app/src/Components/forms/LogIn.js
@@ -1,9 +1,28 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import Card from '../UI/Card';
 import classes from './LogIn.module.css';
 import logInImg from '../../assets/undraw_Login_re_4vu2.png';
 
 const Login = () => {
+    const [enteredEmail, setEnterdEmail] = useState('');
+    const [enteredPassword, setEnteredPassword] = useState('');
+
+    const emailInputChangeHandler = event => {
+        setEnterdEmail(event.target.value);
+    }
+
+    const passwordInputChangeHandler = event => {
+        setEnteredPassword(event.target.value);
+    }
+
+    const formSubmissionHandler = event => {
+        event.preventDefault();
+
+        console.log(enteredEmail, enteredPassword);
+        setEnterdEmail('');
+        setEnteredPassword('');
+    }
+
     const btnClass = `${classes.btn} ${classes['btn-primary']}`;
     const mainContainer = `container ${classes.mainContainer}`
     const subContainer = `row  ${classes.subContainer}`;
@@ -19,14 +38,14 @@ const Login = () => {
                     <div className="col-lg-6 col-md-6 ">
                         <Card className={classes['login-form-container']}>
                             <h2>Sign in to your account</h2>
-                            <form className={classes['login-form']}>
+                            <form className={classes['login-form']} onSubmit={formSubmissionHandler}>
                                 <div className={classes['form-group']}>
                                     <label htmlFor="email">Email address</label>
-                                    <input type="email" className={classes['form-control']} id="email" placeholder="Enter email"/>
+                                    <input value={enteredEmail} type="email" className={classes['form-control']} id="email" placeholder="Enter email" onChange={emailInputChangeHandler}/>
                                 </div>
                                 <div className={classes['form-group']}>
                                     <label htmlFor="password">Password</label>
-                                    <input type="password" className={classes['form-control']} id="password" placeholder="Password"/>
+                                    <input value={enteredPassword} type="password" className={classes['form-control']} id="password" placeholder="Password" onChange={passwordInputChangeHandler}/>
                                 </div>
                                 <button type="submit" className={btnClass}>Sign in</button>
                             </form>
