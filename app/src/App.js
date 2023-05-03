5834030b13350a9bfe55b0c9aa7d95a7af00e7de
Omar <mromarhesham1999@gmail.com>
2023-05-04 02:33:47 +0300

    Login Modal logic is finished

--- app/src/App.js
+++ app/src/App.js
@@ -2,11 +2,20 @@ import React , {useState} from 'react';
 import Login from './Components/forms/LogIn';
 
 function App() {
-  // const [logInIsShown]
+  const [logInIsShown, setLogInIsShown] = useState(false);
+
+  const showLoginHandler = ()=> {
+    setLogInIsShown(true);
+  }
+
+  const hideLoginHandler = ()=> {
+    setLogInIsShown(false);
+  }
 
   return (
    <>
-    <button>Sign In</button>
+    {logInIsShown && <Login onHideLogin={hideLoginHandler}/>}
+    <button onClick={showLoginHandler}>Sign In</button>
     {/* <Login/> */}
    </>
   );
--- app/src/Components/forms/LogIn.js
+++ app/src/Components/forms/LogIn.js
@@ -1,10 +1,11 @@
 import React from "react";
-import Card from '../UI/Card';
 import classes from './LogIn.module.css';
-import logInImg from '../../assets/undraw_Login_re_4vu2.png';
 import useInput from "../../hooks/use-input";
+import Modal from "../UI/Modal";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faXmark } from '@fortawesome/free-solid-svg-icons';
 
-const Login = () => {
+const Login = (props) => {
     const {value: enteredEmail, isValid: enteredEmailIsValid, hasError: emailInputHasError, valueChangeHandler: emailInputChangeHandler, inputBlurHandler: emailInputBlureHandler, reset: resetEmailInput} = useInput(value => value.trim() !== '' || value.includes('@'));
     const {value: enteredPassword, isValid: enteredPasswordIsValid, hasError: passwordInputHasError, valueChangeHandler: passwordInputChangeHandler, inputBlurHandler: passwordInputBlureHandler, reset: resetPasswordInput} = useInput(value => value.trim() !== '' && value.length >= 8);
     
@@ -17,7 +18,7 @@ const Login = () => {
 
     const formSubmissionHandler = event => {
         event.preventDefault();
-        
+
         emailInputBlureHandler();
         passwordInputBlureHandler();
 
@@ -32,45 +33,43 @@ const Login = () => {
     }
 
     const btnClass = `${classes.btn} ${classes['btn-primary']}`;
-    const mainContainer = `container ${classes.mainContainer}`
-    const subContainer = `row  ${classes.subContainer}`;
-
+ 
     // these variables just for controlling the css classes added to the elements
     const emailInputClasses = emailInputHasError ? `${classes['form-group']} ${classes.invalid}` : `${classes['form-group']}`;
     const passwordInputClasses = passwordInputHasError ? `${classes['form-group']} ${classes.invalid}` : `${classes['form-group']}`;
 
     return (
-        <div className={classes.wrapper}>
-            <Card className={mainContainer}>
-                <div className={subContainer}>
-                    <h1 className="text-center mt-5">Welcome back!</h1>
-                    <div className="col-lg-6 col-md-6">
-                        <img className="w-100" src={logInImg} alt="Login Image"/>
-                    </div>
-                    <div className="col-lg-6 col-md-6 ">
-                        <Card className={classes['login-form-container']}>
-                            <h2>Sign in to your account</h2>
-                            <form className={classes['login-form']} onSubmit={formSubmissionHandler}>
-                                <div className={emailInputClasses}>
-                                    <label htmlFor="email">Email address</label>
-                                    <input value={enteredEmail} type="email" className={classes['form-control']} id="email" placeholder="Enter email" onChange={emailInputChangeHandler} onBlur={emailInputBlureHandler}/>
-                                    {emailInputHasError && <p className={classes['error-text']}>Invalid email!</p>}
-                                </div>
-                                <div className={passwordInputClasses}>
-                                    <label htmlFor="password">Password</label>
-                                    <input value={enteredPassword} type="password" className={classes['form-control']} id="password" placeholder="Password" onChange={passwordInputChangeHandler} onBlur={passwordInputBlureHandler}/>
-                                    {passwordInputHasError && <p className={classes['error-text']}>Invalid password!</p>}
-                                </div>
-                                <button type="submit" className={btnClass}>Sign in</button>
-                            </form>
-                            <div className={classes['login-form-footer']}>
-                                <p>Don't have an account yet? <a href="#">Sign up</a></p>
-                            </div>
-                        </Card>
-                    </div>
+        <Modal onClick={props.onHideLogin}>
+            <div className="d-flex justify-content-end">
+                <button onClick={props.onHideLogin}>
+                    <FontAwesomeIcon icon={faXmark}/>
+                </button>
+            </div>
+
+            <div className="mb-5">
+                <h2 className={classes.welcome}>Welcome back!</h2>
+                <h2 className={classes.sign}>Sign in to your account</h2>
+            </div>
+
+            <form className={classes['login-form']} onSubmit={formSubmissionHandler}>
+                <div className={emailInputClasses}>
+                <label htmlFor="email">Email address</label>
+                <input value={enteredEmail} type="email" className={classes['form-control']} id="email" placeholder="Enter email" onChange={emailInputChangeHandler} onBlur={emailInputBlureHandler}/>
+                {emailInputHasError && <p className={classes['error-text']}>Invalid email!</p>}
                 </div>
-            </Card>
-        </div>
+                <div className={passwordInputClasses}>
+                <label htmlFor="password">Password</label>
+                <input value={enteredPassword} type="password" className={classes['form-control']} id="password" placeholder="Password" onChange={passwordInputChangeHandler} onBlur={passwordInputBlureHandler}/>
+                {passwordInputHasError && <p className={classes['error-text']}>Invalid password!</p>}
+                </div>
+                <button type="submit" className={btnClass}>Sign in</button>
+            </form>
+
+            <footer className={classes['login-form-footer']}>
+                <p>Don't have an account yet? <a href="#">Sign up</a></p>
+            </footer>
+
+        </Modal>
         
       );
 };
