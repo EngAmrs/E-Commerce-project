097cc3844a088090dc6bfdd6ab6e2add7cc448c4
Amr Salah <amr.abdoullah@gmail.com>
2023-05-04 19:43:26 +0300

    Home categories is created

--- app/src/Components/Home/Main/Home.js
+++ app/src/Components/Home/Main/Home.js
@@ -1,12 +1,14 @@
 import React, {Fragment} from 'react';
 import CarouselCom from '../Carousel/Carousel'
 import style from './Home.module.css'
+import Categories from '../MainCategories/Categories';
 const Home = () => {
     return ( 
         
         <Fragment>
             <div className={style.home}>
             <CarouselCom/>
+            <Categories/>
             </div>
         </Fragment>
 
