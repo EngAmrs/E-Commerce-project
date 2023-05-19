6ad26b5be756c7bff2410fdae6ddbed50716004c
Amr Salah <amr.abdoullah@gmail.com>
2023-05-20 00:20:27 +0300

    Home products is updated

--- app/src/Components/Home/BestSellers/BestSellers.js
+++ app/src/Components/Home/BestSellers/BestSellers.js
@@ -1,44 +1,73 @@
-import style from './BestSellers.module.css'
+import style from '../../Shop/Products/ProductItems/ProductItemts.module.css'
+import styles from './BestSellers.module.css'
+
 import {Button} from 'react-bootstrap';
-import { BsHeartFill } from "react-icons/bs";
-import { fetchCategories } from '../../../Redux/Slices/Shop/CategoriesSlice'
+import { BsHeart, BsHeartFill } from "react-icons/bs";
+import { fetchAllProducts } from '../../../Redux/Slices/Shop/allProductsSlice'
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+import formattedCurrency from '../../UI/Currency';
+import ProductDetails from '../../Shop/Products/ProductDetails/ProductDetails';
 
 
-const BestSellers = () => {
-    const categories = useSelector((state) => state.categories.categories);
+const BestSellers = ({onProductClick}) => {
+    const products = useSelector((state) => state.allProducts.products);
     const dispatch = useDispatch();
+    const imageUrl = 'http://localhost:8000/'
+    const [showModal, setShowModal] = useState(false);
+    const [selectedProduct, setSelectedProduct] = useState(null);
 
+    const handleProductClick = (product) => {
+      setSelectedProduct(product);
+      setShowModal(true);
+    };
+
+    const handleCloseModal = () => {
+        setSelectedProduct(null);
+        setShowModal(false);
+      };
     useEffect(()=>{
-        dispatch(fetchCategories({limit: 3, page: 1}))
+        dispatch(fetchAllProducts({limit: 3, page: 1}))
     },[dispatch])
-    console.log(categories);
     
     return ( 
-        <div id={style.cards_landscape_wrap_2}>
-        <div className={`${style.container} container`}>
+        <>
+        <div id={`${style.cards_landscape_wrap_2}`}>
+        <div className={`${styles.container} container`}>
         <h1>Best <span>Sellers</span></h1>
             <hr/>
             <div className="row">
-                <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3">
-                    <div className={style.card_flyer}>
-                        <div className={style.text_box}>
-                            <div className={style.image_box}>
-                            <img src={require("../../../assets/Home/banner-01.jpg")} alt="" />
-                            </div>
-                            <div className={`${style.text_container} row`}>
-                                <span className='col-sm-6'>dsadqw</span>
-                                <span className='col-sm-6'>45.55$</span> 
-                                <span className='col-sm-2'><BsHeartFill/></span>       
-                                <Button href='' className={`col-sm-7 ${style.add_to_cart}`}>Add to Cart</Button>                                  
+            {
+                    products.map((product)=> (
+                        <div className="col-xs-12 col-sm-6 col-md-3 col-lg-3" key={product.id}>
+                            <div className={style.card_flyer}>
+                                <div className={style.text_box}>
+                                    <div className={style.image_box}>
+                                    <img onClick={() => handleProductClick(product)} src={`${imageUrl}${product.productPic}`} alt={product.name} />
+                                    </div>
+                                    <div className={`${style.text_container}`}>
+                                        <span>{product.name}</span>
+                                        <span><BsHeart/></span>       
+                                        <span>{formattedCurrency.format(product.price)}</span> 
+                                    </div>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </div>
+                    ))
+                }        
+                
             </div>
         </div>
     </div>
+
+    {showModal && selectedProduct && (
+            <ProductDetails
+                product={selectedProduct}
+                onCloseModal={handleCloseModal}
+                show ={showModal}
+             />
+        )}
+    </>
      );
 }
  
